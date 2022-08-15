b55022cfb8877a960623f4196b9a19a9e7eeb59e
Laurapaulina09 <laura.norenaco@amigo.edu.co>
2022-08-14 21:54:23 -0500

    inicio del chat

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+const path=require('path')
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/crearUsuario',function(req,res,next){
+  res.sendFile(path.join(__dirname,'..','views','crearUsuario.html'))
+}
+)
+
+module.exports = router;
